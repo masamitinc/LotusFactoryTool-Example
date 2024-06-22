f27126a6bf2776f54fefcd02aeb7e2636193780a
Masami Tsukamoto <masamit.inc@gmail.com>
2024-06-22 10:49:46 -0700

    TimeStamp handling and Write to CSV
    - html/javascript
        - setTimeout() to call every 1sec and update `id_timestamp` element if exsist.
        - FocusToFirstInputTextField()
            - After html is loaded, focus to the fiest input text.
    
    - main.py
        - Added to store submitted data to CSV file
        - CSV file name is based on datetime at program execution.
            - ex) `Output file: C:\Dev\LotusFactoryTool\20240622-103102.csv`

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import datetime
 import json
-
+import csv
 
 # Opening JSON file
 f = open('config.json')
@@ -11,10 +11,15 @@ f = open('config.json')
 # a dictionary
 configargs = json.load(f)
 
-TOOL_NAME=configargs['stationtype']
-TOOL_VERSION=configargs['version']
+TOOL_NAME = configargs['stationtype']
+TOOL_VERSION = configargs['version']
 
 app = Flask(__name__)
+
+dt = datetime.datetime.now()
+OUTPUT_FILENAME=os.path.join(app.root_path, f'{dt.year:04}{dt.month:02}{dt.day:02}-{dt.hour:02}{dt.minute:02}{dt.second:02}'+".csv")
+print("Output file:", OUTPUT_FILENAME)
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -25,14 +30,18 @@ def favicon():
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        dt = datetime.datetime.now()
-        print("{}, VISLED:{}, IR:{}, BUTTON:{}, BarCode1:{}, BarCode2:{}".format(
-            str(dt),
-            request.form['result_visled'],
-            request.form['result_irled'],
-            request.form['result_button'],
-            request.form['TextBarCode_1'],
-            request.form['TextBarCode_2']))
+        csv_row = []
+        for i in request.form:
+            # print("{}: {}".format(i, request.form[i]))
+            csv_row.append(request.form[i])
+        csv_row.append(TOOL_NAME)
+        csv_row.append(TOOL_VERSION)
+        print(csv_row)
+
+        # Append
+        with open(OUTPUT_FILENAME, 'a', newline="") as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter=',')
+            csvwriter.writerow(csv_row)
 
     return render_template('index_tx.html', configargs=configargs)
 
